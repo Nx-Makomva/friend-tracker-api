d11864256fc71acf074bc9a9b43d20fcf91486c7
Nic Makomva <nicholasstm@gmail.com>
2024-06-06 15:06:17 +0100

    friend profile service complete

--- src/main/java/com/friend_tracker/friend_tracker/FriendProfileService.java
+++ src/main/java/com/friend_tracker/friend_tracker/FriendProfileService.java
@@ -1,8 +1,10 @@
 package com.friend_tracker.friend_tracker;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -18,7 +20,6 @@ public class FriendProfileService {
     public void addProfile(FriendProfile profile) {
         friendProfileRepository.save(profile);
     }
-
 
     // READ
 
@@ -45,6 +46,15 @@ public class FriendProfileService {
         return friendProfileRepository.getAllByName(profileName);
     }
 
+    public List<FriendProfile> getByBirthdayBetween(LocalDate startDate, LocalDate endDate) {
+        return friendProfileRepository.getAllByBirthdayBetween(startDate, endDate);
+    }
+
+    public List<FriendProfile> getByOccupation(String occupation) {
+        return friendProfileRepository.getAllByOccupation(occupation);
+    }
+
+
     public List<FriendProfile> getAllByHeritage(String heritage, int limit) {
         List<FriendProfile> profiles = friendProfileRepository.getAllByHeritage(heritage);
 
@@ -54,12 +64,32 @@ public class FriendProfileService {
                 .collect(Collectors.toList());
     }
 
-
+    public List<FriendProfile> getByLastSeenBetween(LocalDate startDate, LocalDate endDate) {
+        return friendProfileRepository.getAllByLastSeenBetween(startDate, endDate);
+    }
 
     // UPDATE
 
+    @Transactional
+    public void updateProfile(FriendProfile newProfile, long id) {
+        if (!friendProfileRepository.existsById(id)) {
+            throw new FriendNotFoundException("Profile not Found: Cannot update Profile.");
+        }
+
+        newProfile.setId(id);
+
+        friendProfileRepository.save(newProfile);
+    }
 
     // DELETE
 
+    @Transactional
+    public void deleteProfileById(long id) {
+        if (!friendProfileRepository.existsById(id)) {
+            throw new FriendNotFoundException("Profile not Found: Cannot delete Profile.");
+        }
+
+         friendProfileRepository.deleteProfileById(id);
+    }
 
 }
--- src/main/java/com/friend_tracker/friend_tracker/FriendsController.java
+++ src/main/java/com/friend_tracker/friend_tracker/FriendsController.java
@@ -15,9 +15,31 @@ public class FriendsController {
     FriendProfileService friendProfileService;
     @Autowired
     FriendAllergyService friendAllergyService;
-    @Autowired
-    FriendProfileAllergyMapping friendProfileAllergyMapping;
+//    @Autowired
+//    FriendProfileAllergyMapping friendProfileAllergyMapping;
 
+    @ExceptionHandler
+    public ResponseEntity<String> handleExceptions(FriendNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+    // CREATE
+
+    @PostMapping("/profile")
+    public ResponseEntity<FriendProfile> createProfile(@RequestBody FriendProfile profile) {
+        friendProfileService.addProfile(profile);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(profile);
+    }
+
+    @PostMapping("/allergy")
+    public ResponseEntity<FriendAllergy> createAllergy(@RequestBody FriendAllergy allergy) {
+//        friendAllergyService.addAllergy(allergy);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(allergy);
+    }
+
+
+    // READ
     @GetMapping("/profiles")
     public ResponseEntity<List<FriendProfile>> getProfiles(@RequestParam(required = false) String profileName, @RequestParam(defaultValue = "10") int limit) {
 
@@ -38,4 +60,12 @@ public class FriendsController {
         return ResponseEntity.status(HttpStatus.OK).body(friendAllergyService.getAllAllergies(limit)) ;
 
     }
+
+
+    // UPDATE
+
+
+
+
+    // DELETE
 }
